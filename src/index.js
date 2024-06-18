abcdd313890cfcdd78a2776e5aa36fae7e1188c7
CarsonLenze <carson@overfuel.com>
2024-06-18 16:48:34 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const crypto = require('crypto');
+const Channel = require('./channel.js');
+const setup = require('./setup.js');
 const API = require('./api.js');
 const fs = require('fs');
 
-const VERSION = '0.15.13'
+global.VERSION = '0.15.13'
 
 const server = {
     host: '127.0.0.1',
@@ -11,26 +12,24 @@ const server = {
 }
 
 async function run() {
-    //playit.gg Secret
+    const api = new API();
     let secret;
 
     try {
         let buffer = fs.readFileSync('./secret.txt');
         secret = buffer.toString();
     } catch (err) {
-        secret = await setup();
+        secret = await setup.claim_secret(api);
         if (secret) fs.writeFileSync('./secret.txt', Buffer.from(secret));
     }
 
-    const api = new API(secret);
+    //set api secret
+    api.setSecret(secret);
 
     const rundata = await api.agents_rundata();
     if (rundata.status !== 'success') return console.error(rundata);
 
     if (rundata.data.account_status !== 'ready') return console.error('account is not ready');
-    
-    //do tunnel creation here if one does not exsist
-
     const agent_id = rundata.data.agent_id;
 
     const tunnels = await api.list_tunnels(agent_id);
@@ -55,6 +54,12 @@ async function run() {
     //console.log(routing.data)
     const address = routing.data.targets4[0];
     console.log(address)
+    // const channel = new Channel('209.25.140.1');
+    // channel.start();
+
+    // channel.onMessage((msg, rinfo) => {
+    //     console.log(msg, rinfo)
+    // })
 }
 
 
@@ -65,70 +70,5 @@ async function run() {
     // if (routing.status !== "success") return console.trace(routing);
     // console.log(routing)
 */
-
-async function setup() {
-    const claimCode = crypto.randomBytes(8).toString('hex');
-    const api = new API();
-
-    const body = {
-        code: claimCode,
-        agent_type: "self-managed",
-        version: VERSION
-    }
-
-    const claim = await api.claim_setup(body);
-    if (claim.status !== 'success') return console.error(claim);
-
-    console.log('Visit web page:', 'https://playit.gg/mc/' + body.code);
-    console.log('Or enter code: ', body.code);
-    console.log('\x1b[1m%s\x1b[0m', `Setup playit.gg (v${VERSION})`);
-
-    return new Promise((resolve, reject) => {
-        const interval = setInterval(async () => {
-            const claim = await api.claim_setup(body);
-
-            if (claim.status !== 'success') {
-                clearInterval(interval);
-                console.error(claim);
-                return process.exit(1);
-            }
-
-            switch (claim.data) {
-                case 'WaitingForUserVisit':
-                    /* waiting for user to visit */
-                break;
-                case 'WaitingForUser':
-                    /* waiting for user to accept */
-                break;
-                case 'UserAccepted':
-                    //user accepted
-                    clearInterval(interval);
-                    console.log('Authenticating');
-
-                    const exchange = await api.claim_exchange(body.code);
-
-                    if (exchange.status !== 'success') {
-                        console.error(claim);
-                        return process.exit(1);
-                    }
-
-                    console.clear();
-                    resolve(exchange.data.secret_key);
-                break;
-                case 'UserRejected':
-                    /* user rejected */
-                    clearInterval(interval);
-                    console.error('User rejected agent');
-                    process.exit(1);
-                break;
-                default:
-                    clearInterval(interval);
-                    console.error('unknown claim code', claim.data);
-                    return process.exit(1);
-                break;
-            }
-        }, 5000);
-    });
-}
 
 run()
---
+++ src/setup.js
@@ -0,0 +1,70 @@
+const crypto = require('crypto');
+
+async function claim_secret(api) {
+    const claimCode = crypto.randomBytes(8).toString('hex');
+
+    const body = {
+        code: claimCode,
+        agent_type: "self-managed",
+        version: global.VERSION
+    }
+
+    const claim = await api.claim_setup(body);
+    if (claim.status !== 'success') return console.error(claim);
+
+    console.log('Visit web page:', 'https://playit.gg/mc/' + body.code);
+    console.log('Or enter code: ', body.code);
+    console.log('\x1b[1m%s\x1b[0m', `Setup playit.gg (v${global.VERSION})`);
+
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(async () => {
+            const claim = await api.claim_setup(body);
+
+            if (claim.status !== 'success') {
+                clearInterval(interval);
+                console.error(claim);
+                return process.exit(1);
+            }
+
+            switch (claim.data) {
+                case 'WaitingForUserVisit':
+                    /* waiting for user to visit */
+                break;
+                case 'WaitingForUser':
+                    /* waiting for user to accept */
+                break;
+                case 'UserAccepted':
+                    //user accepted
+                    clearInterval(interval);
+                    console.log('Authenticating');
+
+                    const exchange = await api.claim_exchange(body.code);
+
+                    if (exchange.status !== 'success') {
+                        console.error(claim);
+                        return process.exit(1);
+                    }
+
+                    console.clear();
+                    resolve(exchange.data.secret_key);
+                break;
+                case 'UserRejected':
+                    /* user rejected */
+                    clearInterval(interval);
+                    console.error('User rejected agent');
+                    process.exit(1);
+                break;
+                default:
+                    clearInterval(interval);
+                    console.error('unknown claim code', claim.data);
+                    return process.exit(1);
+                break;
+            }
+        }, 5000);
+    });
+}
+
+
+module.exports = {
+    claim_secret: claim_secret
+}
